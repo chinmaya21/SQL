60aeac83c44cd59c2b0dc9db6f802c0b99d03bc4
chinmayabarik <barikchinmaya808@gmail.com>
2026-06-28 22:24:00 +0530

    Sql Probelms of Leetcode Sql50

--- 1661_Average_Time_of_Process_per_Machine.sql
+++ 1661_Average_Time_of_Process_per_Machine.sql
@@ -1,1 +1,8 @@
 1661_Average_Time_of_Process_per_Machine.sql
+
+# Write your MySQL query statement below
+select machine_id,
+round(sum(case when activity_type='start' then timestamp * -1 
+else timestamp * 1 end)/(count(distinct process_id)),3) as processing_time
+from Activity 
+group by 1
--- 197_Rising_Temperature.sql
+++ 197_Rising_Temperature.sql
@@ -1,2 +1,14 @@
 197_Rising_Temperature.sql
 
+# Write your MySQL query statement below
+
+select id from
+(select 
+id,
+recordDate,
+temperature,
+lag(recordDate) over(order by recordDate asc) as prev_day,
+lag(temperature) over(order by recordDate asc) as prev_temp
+from 
+Weather)D
+where datediff(recordDate,prev_day)=1 and temperature > prev_temp
